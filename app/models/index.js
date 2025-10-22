5db4fe5a21e8a37ebe24e815f7a7b51486bdb6e9
Breno Corsi <breno.corsi@gmail.com>
2025-10-22 18:58:33 -0300

    feat(index.js): script create db and tables\nfeat(tutorial.model.js): example model for future table creations\nchore(package.json): remove mysql, add sequelize

---
+++ app/models/index.js
@@ -0,0 +1,31 @@
+// /app/models/index.js
+
+/*
+    This script initializes the database with the specified tables.
+    > node ./app/models/index.js
+*/
+
+require("dotenv").config();
+
+const { execSync } = require('child_process');
+const Sequelize = require("sequelize");
+
+let command = `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`;
+command = `mysql -u ${process.env.USER} -p${process.env.PASSWORD} -e "${command}"`;
+execSync(command);
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+db.sequelize.sync();
+
+module.exports = db;
